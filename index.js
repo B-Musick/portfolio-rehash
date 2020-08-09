e0b9ce38415769bde08330e0665ae8d0240cf2de
B-Musick <bmuze1@gmail.com>
2020-08-09 10:10:24 -0500

    Made JS object oriented

--- index.js
+++ index.js
@@ -1,134 +1,183 @@
-let topicsContainer = document.getElementById("topic-links-container");
+class Topic{
+    // Oulines both the Word and the rectangle associated
+    constructor(index, topic){
+        this.topics = ["technology", "courses", "learning", "projects", "about"];
+        this.topic = topic;
+        this.index = index;
+        this.topicBaseSpacingFromBottom = 14; // Represents percent
+        this.verticalSpacing = 6;
+        this.rectBaseWidth = 18;
+        this.horizontalWordSpacingMultiplier = 5;
+        this.container = document.getElementById("topic-links-container");
 
-let topHalf = document.getElementById("top");
-let nameIcon = document.getElementById("name-icon");
-let titleIcon = document.getElementById("title-icon");
+        if (this.constructor == Topic) {
+            throw new Error("Abstract classes can't be instantiated.");
+        }
+    }
+}
 
-let currentTopicTitleContainer = document.getElementById("current-topic-title");
+class WordRect extends Topic{
+    constructor(index, topic) {
+        super(index, topic);
+        this.rect = document.createElement("div");
+        this.createRectAssociatedWithLink();
+    }
 
-/********************** PRINT TOPICS **************************/
-let topics = ["technology", "courses", "learning", "projects","about"];
+    createRectAssociatedWithLink() {
+        // Create the rect associated with the link
+        this.rect.className = "info-rect";
+        this.rect.id = this.topic + "-rect";
 
-// Spacing
-let topicBaseSpacingFromBottom = 14; // Represents percent
-let verticalSpacing = 6;
-let rectBaseWidth = 18;
-let horizontalWordSpacingMultiplier = 5;
-let wordRectSpacing = 1;
-let fontSize = 4;
+        // Add CSS styling
+        this.rect.style.width = this.rectBaseWidth + (this.horizontalWordSpacingMultiplier * (this.topics.length - this.index)) + "%";
+        this.rect.style.bottom = this.topicBaseSpacingFromBottom + this.verticalSpacing * (this.index % this.topics.length) + "%";
 
-topics.forEach((topic,index)=>{
-    // Loop through the topics and add a rect and associated topic name
+        this.container.appendChild(this.rect); // Add to the container
+    }
+}
 
-    // Create the rect associated with the link
-    let rect = document.createElement("div");
-    rect.className = "info-rect";
-    // rect.id = topic+"-rect";
+class TopicLink extends Topic {
+    constructor(index, topic){
+        super(index, topic);
+        this.wordRectSpacing = 1;
+        this.fontSize = 4;
+        // Create the link associated with the rect
+        this.link = document.createElement("a");
+        this.linkTopic = document.createElement("div");
+        this.createLinkName();
+    }
 
-    // Add CSS styling
-    rect.style.width = rectBaseWidth+(horizontalWordSpacingMultiplier*(topics.length-index))+"%";
-    rect.style.bottom = topicBaseSpacingFromBottom+verticalSpacing*(index%topics.length)+"%";
+    createLinkName(){
+        // Add class name to link name
+        this.linkTopic.className = "topic-link";
+        this.linkTopic.id = this.topic + "-link";
 
-    topicsContainer.appendChild(rect); // Add to the container
+        this.linkTopic.style.bottom = this.topicBaseSpacingFromBottom + this.verticalSpacing * (this.index % this.topics.length) - this.fontSize / 2 + "%";
+        this.linkTopic.style.right = this.rectBaseWidth + (this.horizontalWordSpacingMultiplier * (this.topics.length - this.index)) + this.wordRectSpacing + "%";
 
-    // Create the link associated with the rect
-    let link = document.createElement("a");
-    let linkTopic = document.createElement("div");
+        this.linkTopic.innerHTML = this.topic.toUpperCase();
+        this.link.appendChild(this.linkTopic);
 
-    // Add class name to link name
-    linkTopic.className = "topic-link";
-    // linkTopic.id = topic+"-link";
+        this.container.appendChild(this.link); // Add to the container
+    }
+}
 
-    linkTopic.style.bottom = topicBaseSpacingFromBottom + verticalSpacing * (index % topics.length) - fontSize/2 + "%";
-    linkTopic.style.right = rectBaseWidth + (horizontalWordSpacingMultiplier *(topics.length-index))+wordRectSpacing+"%";
+class TopicHandler extends Topic{
+    constructor(){
+        super();
+        this.printTopicAndRect();
+        this.topicLinks = document.querySelectorAll(".topic-link");
+        this.topicLinksRects = document.querySelectorAll(".info-rect");
+        this.topHalf = document.getElementById("top");
+        this.nameIcon = document.getElementById("name-icon");
+        this.titleIcon = document.getElementById("title-icon");
+        this.clickedLink = false;
+        this.currentTopicTitleContainer = document.getElementById("current-topic-title");
+        this.clickedTopicLogic();
+    }
 
-    linkTopic.innerHTML = topic.toUpperCase();
-    link.appendChild(linkTopic);
+    printTopicAndRect(){
+        this.topics.forEach((topic, index) => {
+            // Loop through the topics and add a rect and associated topic name
+            this.link = new TopicLink(index, topic);
 
-    topicsContainer.appendChild(link); // Add to the container
-})
+            this.rect = new WordRect(index, topic);
+        })
+    }
 
-// This needs to be after topic creating since .topic-link wont exist till JS creates them
-let topicLinks = document.querySelectorAll(".topic-link");
-let topicLinksRects = document.querySelectorAll(".info-rect");
+    clickedTopicLogic(){
+        /********************** CLICK TOPIC LOGIC **************************/
+        this.topicLinks.forEach(link => {
+            let linkName = link.id.split('-')[0];
+            // let topRetractSizeId = linkName +"-retract-top-size";
 
-/********************** CLICK TOPIC LOGIC **************************/
-topicLinks.forEach(link=>{
-    let linkName = link.id.split('-')[0];
-    // let topRetractSizeId = linkName +"-retract-top-size";
+            link.addEventListener("click", e => {
+                this.clickedLink = true;
+                this.moveCurrentClickedValueToHeader(linkName);
+                this.removeClickedLink(linkName,link);
+                this.retractTopNameAndTitle();
+                this.labelNotClicked(e.target.id);
+            });
 
-    link.addEventListener("click",e=>{
-        // Add name to current topic as the title in top left corner 
-        currentTopicTitleContainer.textContent = linkName.toUpperCase();
+            this.mouseOverLinkEvent(link);
+            this.mouseOutLinkEvent(link);
+        })
+    };
 
-        // Animation set so current title is moved to the title location
-        currentTopicTitleContainer.classList.add("set-current-topic-title-top-left");
+    retractTopNameAndTitle(){
+        this.topHalf.classList.add("top-retract-animation");
+        this.nameIcon.classList.add("name-icon-animation");
+        this.titleIcon.classList.add("title-icon-animation");
+    }
 
-        topHalf.classList.add("top-retract-animation");
-
+    removeClickedLink(linkName,link){
         // Remove rectangle of clicked link
         let linkRect = document.getElementById(linkName + "-rect");
         linkRect.classList.add("rect-remove");
-
-        nameIcon.classList.add("remove-titles-animation");
-        titleIcon.classList.add("remove-titles-animation");
-
-        nameIcon.classList.add("name-icon-animation");
-        titleIcon.classList.add("title-icon-animation");
-
         link.classList.add("topic-link-remove-animation");
+    }
 
-        labelNotClicked(topicLinks,e.target.id);
-        
-    });
+    moveCurrentClickedValueToHeader(linkName){
+        // Add name to current topic as the title in top left corner 
+        this.currentTopicTitleContainer.textContent = linkName.toUpperCase();
 
-    link.addEventListener("mouseover",e=>{
-        let linkHovered = document.getElementById(e.target.id);
-        linkHovered.style.color = "yellow";
+        // Animation set so current title is moved to the title location
+        this.currentTopicTitleContainer.classList.add("set-current-topic-title-top-left");
+    }
+    mouseOutLinkEvent(link) {
+        link.addEventListener("mouseout", e => {
+            // If just hovered then return links to normal
+            if (!this.clickedLink) this.returnLablesToNormal();
+        })
+    }
 
-        labelNotHovered(topicLinks,e.target.id)
-    })
+    mouseOverLinkEvent(link){
+        link.addEventListener("mouseover", e => {
+            let linkHovered = document.getElementById(e.target.id);
+            linkHovered.style.color = "yellow";
 
-    link.addEventListener("mouseout", e => {
-        returnLablesToNormal(topicLinks, e.target.id)
-    })
-});
+            this.labelNotHovered(e.target.id)
+        })
+    }
 
+    labelNotClicked(clicked){
+        /* Called when label clicked, if not the label which is clicked, then fold
+        it in */
+        this.topicLinks.forEach(topic => {
+            console.log(clicked)
+            if (topic.id != clicked) {
+                topic.classList.add("label-folded");
 
-let labelNotClicked = (labelValues,clicked) => {
-    /* Called when label clicked, if not the label which is clicked, then fold
-    it in */
-    labelValues.forEach(topic=>{
+                let rect = document.getElementById(topic.id.split("-")[0] + "-rect");
+                rect.classList.add("rect-folded");
+            }
+        });
+    }
 
-        if(topic.id!=clicked){
-            topic.classList.add("label-folded");
+    labelNotHovered (clicked){
+        this.topicLinks.forEach(topic => {
 
-            let rect = document.getElementById(topic.id.split("-")[0] + "-rect");
-            rect.classList.add("rect-folded");
-        }
-    });
-}
+            if (topic.id != clicked) {
+                topic.classList.add("label-folded");
 
-let labelNotHovered = (labelValues,clicked)=>{
-    labelValues.forEach(topic => {
+                let rect = document.getElementById(topic.id.split("-")[0] + "-rect");
+                rect.classList.add("rect-folded");
+            }
+        });
+    }
 
-        if (topic.id != clicked) {
-            topic.classList.add("label-folded");
+    returnLablesToNormal(){
+        this.topicLinks.forEach(topic => {
 
-            let rect = document.getElementById(topic.id.split("-")[0] + "-rect");
-            rect.classList.add("rect-folded");
-        }
-    });
-}
-
-let returnLablesToNormal = (labelValues)=>{
-    labelValues.forEach(topic => {
-       
             topic.classList.remove("label-folded");
             topic.style.color = "rgb(72, 72, 66)"; // Return color to grey
 
             let rect = document.getElementById(topic.id.split("-")[0] + "-rect");
             rect.classList.remove("rect-folded");
+
+        });
+    }
         
-    });
 }
+
+let topicHandler = new TopicHandler();
